163261bcec9de06eab944e635abb345e6a9db22e
Mikheil Gotiashvili <mrGott@Mikheils-Mac-mini.local>
2016-10-02 01:15:25 +0400

    Added Image and UITextView to Page Cell

--- Audible/PageCell.swift
+++ Audible/PageCell.swift
@@ -23,9 +23,20 @@ class PageCell: UICollectionViewCell {
     let stepImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.translatesAutoresizingMaskIntoConstraints = false
-        imageView.backgroundColor = .green
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.image = UIImage(named: "page1")
         
         return imageView
+    }()
+    
+    let bottomView: UITextView = {
+        let bottomView = UITextView()
+        bottomView.translatesAutoresizingMaskIntoConstraints = false
+        bottomView.backgroundColor = .white
+        bottomView.text = "Hello There"
+        
+       return bottomView
     }()
     
     func setupCell(){
@@ -33,9 +44,15 @@ class PageCell: UICollectionViewCell {
         backgroundColor = .green
         addSubview(stepImageView)
         stepImageView.topAnchor.constraint(equalTo: topAnchor).isActive = true
-        stepImageView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
         stepImageView.leftAnchor.constraint(equalTo: leftAnchor).isActive = true
         stepImageView.rightAnchor.constraint(equalTo: rightAnchor).isActive = true
+        stepImageView.heightAnchor.constraint(equalTo: heightAnchor, multiplier: 0.7).isActive = true
+        
+        addSubview(bottomView)
+        bottomView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+        bottomView.heightAnchor.constraint(equalTo: heightAnchor, multiplier: 0.3).isActive = true
+        bottomView.leftAnchor.constraint(equalTo: leftAnchor).isActive = true
+        bottomView.rightAnchor.constraint(equalTo: rightAnchor).isActive = true
         
     }
     
